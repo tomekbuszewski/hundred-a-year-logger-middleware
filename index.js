9b16e0dddd73effbeb48fef2e6dca72ced7719aa
Tomek Buszewski <tomek.buszewski@gmail.com>
2020-08-10 20:46:26 +0200

    package

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-import morgan from "morgan";
-import { createStream } from "rotating-file-stream";
-import { join } from "path";
-const accessLogStream = (filename, path = "logs", interval = "1d") => createStream(`${filename}.log`, {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.errorLog = exports.accessLog = void 0;
+const morgan_1 = __importDefault(require("morgan"));
+const rotating_file_stream_1 = require("rotating-file-stream");
+const path_1 = require("path");
+const accessLogStream = (filename, path = "logs", interval = "1d") => rotating_file_stream_1.createStream(`${filename}.log`, {
     interval,
-    path: join(path),
+    path: path_1.join(path),
 });
 const accessLog = (filename, format = "combined") => {
-    return morgan(format, { stream: accessLogStream(filename) });
+    return morgan_1.default(format, { stream: accessLogStream(filename) });
 };
+exports.accessLog = accessLog;
 const errorLog = (format = "combined") => {
-    return morgan(format, {
+    return morgan_1.default(format, {
         skip: (req, res) => res.statusCode < 400,
     });
 };
-export { accessLog, errorLog };
+exports.errorLog = errorLog;
 //# sourceMappingURL=index.js.map
